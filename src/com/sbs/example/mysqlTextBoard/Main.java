6e4d8d183f6ae0bebea3e934e3ebe0d3fff15c3f
LHG313 <dd31391@gmail.com>
2020-12-30 09:51:18 +0900

    http request 후 응답 받아오는 함수 구현

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -1,7 +1,20 @@
 package com.sbs.example.mysqlTextBoard;
 
+import com.sbs.example.mysqlTextBoard.util.Util;
+
 public class Main {
 	public static void main(String[] args) {
 		new App().run();
+
+//		testApi();
+
+	}
+
+	private static void testApi() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		String rs = Util.callApi(url, "api_key=FHq3NgPDalTrO2rcA3f69BSETX8dYqpSyAZ5fsSyGVFQpx5S7L33VeR6YTihgjCh",
+				"forum=my-smg", "thread:ident=article_detail_2.html");
+		System.out.println(rs);
+
 	}
 }
