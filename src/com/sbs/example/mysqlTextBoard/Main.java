e9626595424845a7f325f725a5fadd56966f5bb0
LHG313 <dd31391@gmail.com>
2020-12-30 13:50:17 +0900

    잭슨 라이브러리 추가

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -1,20 +1,13 @@
 package com.sbs.example.mysqlTextBoard;
 
+import com.sbs.example.mysqlTextBoard.test.TestRunner;
 import com.sbs.example.mysqlTextBoard.util.Util;
 
 public class Main {
 	public static void main(String[] args) {
-		new App().run();
+//		new App().run();
 
-//		testApi();
-
-	}
-
-	private static void testApi() {
-		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
-		String rs = Util.callApi(url, "api_key=FHq3NgPDalTrO2rcA3f69BSETX8dYqpSyAZ5fsSyGVFQpx5S7L33VeR6YTihgjCh",
-				"forum=my-smg", "thread:ident=article_detail_2.html");
-		System.out.println(rs);
+		new TestRunner().run();
 
 	}
 }
